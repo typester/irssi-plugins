86257d6e14b71e4c1582a58c92006f32bccd1485
Daisuke Murase <typester@cpan.org>
2012-03-23 19:23:31 +0900

    ソケットをわけた

--- channel-socket.pl
+++ channel-socket.pl
@@ -9,20 +9,21 @@ use AnyEvent;
 use AnyEvent::Socket;
 use AnyEvent::Handle;
 
-my $sock;
+my ($getsock, $setsock);
 
-Irssi::settings_add_str('channel-socket', 'channel-socket-path', '/tmp/irssi-channel.sock');
+Irssi::settings_add_str('channel-socket', 'channel-get-socket-path', '/tmp/irssi-channels.sock');
+Irssi::settings_add_str('channel-socket', 'channel-set-socket-path', '/tmp/irssi-set-channel.sock');
 
 Irssi::command_bind('channel-socket', sub {
     my ($data) = @_;
 
     if ($data eq 'start') {
-        if ($sock) {
+        if ($getsock || $setsock) {
             Irssi::print("socket already started");
             return;
         }
 
-        $sock = tcp_server 'unix/', Irssi::settings_get_str('channel-socket-path'), sub {
+        $getsock = tcp_server 'unix/', Irssi::settings_get_str('channel-get-socket-path'), sub {
             my ($fh) = @_;
 
             my $h; $h = AnyEvent::Handle->new(
@@ -32,29 +33,31 @@ Irssi::command_bind('channel-socket', sub {
                 },
             );
 
-            my $t; $t = AnyEvent->timer( after => 1, cb => sub { undef $h; $t });
+            for my $channel (Irssi::channels()) {
+                $h->push_write("$channel->{name}\n") if $h;
+            }
+            $h->on_drain(sub { undef $h }) if $h;
+        };
+
+        $setsock = tcp_server 'unix/', Irssi::settings_get_str('channel-set-socket-path'), sub {
+            my ($fh) = @_;
+
+            my $h; $h = AnyEvent::Handle->new(
+                fh => $fh,
+                on_error => sub { undef $h },
+            );
 
             $h->push_read( line => sub {
-                my $channel = Irssi::channel_find($_[1]);
-                undef $t;
-
-                if ($channel) {
-                    Irssi::command('window goto ' . $channel->{name});
-                    undef $h;
-                }
-                else {
-                    for my $channel (Irssi::channels()) {
-                        $h->push_write("$channel->{name}\n");
-                    }
-                    $h->on_drain(sub { undef $h });
-                }
+                Irssi::command('window goto ' . $_[1]);
+                undef $h;
             });
         };
 
         Irssi::print("started socket at: " . Irssi::settings_get_str('channel-socket-path'));
     }
     elsif ($data eq 'stop') {
-        undef $sock;
+        undef $setsock;
+        undef $getsock;
         Irssi::print("stopped");
     }
 });
